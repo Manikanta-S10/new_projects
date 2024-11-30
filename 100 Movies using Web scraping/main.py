153f6bd174a1275679ff2e61cfa7a027bca10bd7
Manikanta-S10 <manikantasmani10@gmail.com>
2024-11-30 11:59:50 +0530

    web scraping

--- 100 Movies using Web scraping/main.py
+++ 100 Movies using Web scraping/main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+#url of the target
 URL_ENDPOINT = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
 response = requests.get(url=URL_ENDPOINT)
@@ -9,6 +10,7 @@ code = response.text
 soup = BeautifulSoup(code,"html.parser")
 title_list =[ title.getText() for title in soup.find_all(name="h3",class_="title")]
 
+# file handling 
 with open("movie.text",mode="a",encoding='utf-8') as file:
     for movie in title_list[::-1]:
         file.write(movie + '\n')
