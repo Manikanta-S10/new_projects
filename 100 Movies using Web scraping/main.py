0b1d190b3fecd6186fc1b6539c0afb59abd5c098
Manikanta-S10 <manikantasmani10@gmail.com>
2024-11-30 11:45:09 +0530

    Top 100 movies using Web scraping and file handling

---
+++ 100 Movies using Web scraping/main.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL_ENDPOINT = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(url=URL_ENDPOINT)
+code = response.text
+
+soup = BeautifulSoup(code,"html.parser")
+title_list =[ title.getText() for title in soup.find_all(name="h3",class_="title")]
+
+with open("movie.text",mode="a",encoding='utf-8') as file:
+    for movie in title_list[::-1]:
+        file.write(movie + '\n')
+
+
+
